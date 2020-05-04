740808078cd356adbd773f4733f18437cfebae21
Adam Altmark <adamaltmark@gmail.com>
2020-05-04 21:45:44 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
-function basicTeenager(age)
+function basicTeenager(age) {
   if (age < 20 && age > 12) {
     return "You are a teenager!"
   }
-
+}
 
 function teenager(age) {
   if (age < 20 && age > 12) {
